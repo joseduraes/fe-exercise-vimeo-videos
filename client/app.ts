9af76577708cf3cb531ecd2680586501dc61e318
José Durães <jdmduraes@gmail.com>
2016-10-29 03:01:12 +0200

    base template

--- client/app.ts
+++ client/app.ts
@@ -1,18 +1,27 @@
 import * as Vue from 'vue';
 
+type selector = string;
+
+interface Templates {
+    videoSearch: selector
+}
+
 export class App {
 
     private app: Vue;
-    private root: HTMLElement;
+    private root: selector;
+    private templates: Templates;
 
-    constructor(root: HTMLElement){
-        this.root = root; 
+    constructor(params:{root: selector, templates: Templates}){
+        this.root = params.root; 
+        this.templates = params.templates;
         this.init();
     }
 
     private init(){
         this.app = new Vue({
             el: this.root,
+            template: this.templates.videoSearch,
             data: {
             } 
         });
--- client/main.ts
+++ client/main.ts
@@ -1,5 +1,11 @@
 import 'whatwg-fetch';
 import { App } from './app';
 import 'bootstrap/dist/css/bootstrap.css';
+import './styles/app.css';
 
-const app = new App(document.getElementById('#app'));
+const app = new App({
+    root: '#app',
+    templates: {
+        videoSearch: '#video-search-template'
+    }
+});
